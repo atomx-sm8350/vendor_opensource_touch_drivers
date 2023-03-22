6dcafc337bfaadca01ba671ca2ac8eceb4d98a0a
Simranjeet Thind <quic_sthind@quicinc.com>
2023-03-22 16:09:59 -0700

    touch: goodix: Replacing disable_irq with disable_irq_nosync
    disable_irq ensures existing instances of the IRQ handler have
    completed before returning which may create a deadlock if the handler
    doesn't find the resource.
    
    Change-Id: I7010d2a415f97de5ece8119b855c169ffba665b7
    Signed-off-by: Simranjeet Thind <quic_sthind@quicinc.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- goodix_berlin_driver/goodix_brl_hw.c
+++ goodix_berlin_driver/goodix_brl_hw.c
@@ -327,11 +327,11 @@ static int brl_irq_enbale(struct goodix_ts_core *cd, bool enable)
 	}
 
 	if (!enable && atomic_cmpxchg(&cd->irq_enabled, 1, 0)) {
-		disable_irq(cd->irq);
+		disable_irq_nosync(cd->irq);
 		ts_info(dev, "Irq disabled");
 		return 0;
 	}
-	ts_info(dev, "warnning: irq deepth inbalance!");
+	ts_info(dev, "warning: irq depth imbalance!");
 	return 0;
 }
 
